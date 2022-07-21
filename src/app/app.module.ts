8f121576d62ed0b2592fecce4406993db47571de
Venkatesh Reddy <vr79097@gmail.com>
2022-07-21 21:31:52 +0530

    uploading my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeaturesComponent } from './features/features.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { PressComponent } from './press/press.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { CallToActionComponent } from './call-to-action/call-to-action.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FeaturesComponent,
+    TestimonialsComponent,
+    PressComponent,
+    PricingComponent,
+    CallToActionComponent
   ],
   imports: [
     BrowserModule,
